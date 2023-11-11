cbd81d14033bf08f24681ee6eb6c63f5f420e887
linuxfight <kiko13024@gmail.com>
2023-11-11 21:20:54 +0300

    Add Gallery

--- CinemaApp/Controllers/FilmController.cs
+++ CinemaApp/Controllers/FilmController.cs
@@ -7,6 +7,13 @@ namespace CinemaApp.Controllers
 {
 	public class FilmController : Controller
 	{
+		private IWebHostEnvironment webHostEnvironment;
+
+		public FilmController(IWebHostEnvironment webHostEnvironment)
+		{
+			this.webHostEnvironment = webHostEnvironment;
+		}
+
 		public IActionResult Index()
 		{
 			return View(DataCinema.Films);
@@ -51,5 +58,43 @@ namespace CinemaApp.Controllers
 			DataCinema.Films.Add(film);
             return RedirectToAction("Index");
         }
+
+		public IActionResult Gallery() 
+		{
+			var picturesFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+			var photos = Directory.GetFiles(picturesFolder);
+			return View(photos);		
+		}
+
+		public IActionResult UploadPicture()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Upload(IFormFile file)
+		{
+			if (file != null && file.Length > 0)
+			{
+				var extension = Path.GetExtension(file.FileName);
+				var fileName = Guid.NewGuid().ToString() + extension;
+				var uploadFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+				var path = Path.Combine(uploadFolder, fileName);
+				var stream = new FileStream(path, FileMode.Create);
+				file.CopyTo(stream);
+				stream.Close();
+			}
+
+			return RedirectToAction("UploadPicture");
+		}
+
+		[HttpPost]
+		public IActionResult DeleteImage(string name)
+		{
+			var path = Path.Combine(webHostEnvironment.WebRootPath, "images", name);
+			if (Path.Exists(path))
+				System.IO.File.Delete(path);
+			return RedirectToAction("Gallery");
+		}
 	}
 }
